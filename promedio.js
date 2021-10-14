676fe53134eac2e07ec3a5da88197764a4abd75b
Samuel Quinzada <sqes26@gmail.com>
2021-10-14 00:24:50 -0500

    Promedio, moda y mediana

---
+++ promedio.js
@@ -0,0 +1,13 @@
+function calcularMedia(lista) {
+
+const sumaLista = lista.reduce(
+    function (valorAcumulado = 0, nuevoElemento) {
+        return valorAcumulado + nuevoElemento;
+    }
+);
+
+const promedioLista = sumaLista / lista.length;
+
+return promedioLista;
+}
+
